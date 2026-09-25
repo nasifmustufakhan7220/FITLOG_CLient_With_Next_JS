ca84de044ed5dfee18bdbe6112bf070162cd60c4
Nasif Khan <nasifmustufakhan7220@gmail.com>
2026-09-26 05:04:45 +0600

    Working on env file to deploy the project

--- src/lib/GetAlllWorksOut.ts
+++ src/lib/GetAlllWorksOut.ts
@@ -1,12 +1,12 @@
 export const getExercises = async()=>{
-        const res = await fetch("https://api.abcz.workers.dev/api/fitlog", {cache: "no-store",});
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}`, {cache: "no-store",});
         if(!res.ok) throw new Error("Failed to fetch exercises")
         return res.json();
     
 }
 
 export const getExerciseDetails = async(id:number)=>{
-        const res = await fetch(`https://api.abcz.workers.dev/api/fitlog/${id}`, {next:{revalidate: 60}});
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/${id}`, {next:{revalidate: 60}});
         if(!res.ok) throw new Error("Failed to fetch exercise details");
         return res.json();
 }
